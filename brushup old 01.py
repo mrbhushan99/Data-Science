3058e30285fa9cd401c7bc601e529c5ba050e151
mrbhushan99 <mr.bhushan405@gmail.com>
2026-02-26 20:40:39 +0530

    Just rivision of basics

---
+++ brushup old 01.py
@@ -0,0 +1,24 @@
+a=10
+b=a
+print(a+b)
+
+
+import keyword
+keyword.kwlist
+          #  you will get all the 35 keywords in the python
+
+# Mutiline variables
+
+a,b,c,d = 10,20,30,40
+# NUMBER OF VARIABLES = NUMBER OF VALUES 
+
+print("memory location of a",id(a))
+print("memory location of 10",id(10))
+print("memory location of b",id(b))
+print("memory location of 20",id(20))
+print("memory location of c",id(c))
+print("memory location of 30",id(30))
+print("memory location of d",id(d))
+print("memory location of 40",id(40))
+
+
