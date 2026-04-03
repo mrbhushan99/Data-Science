1698b31cf0f964fa193092c16e74b38347300f1b
mrbhushan99 <mr.bhushan405@gmail.com>
2026-04-03 23:13:08 +0530

    practice

---
+++ break_pass_continue(03_04_26.py
@@ -0,0 +1,33 @@
+"""for i in range(1,6):
+    if i==3:
+        break
+    print(i)
+
+o/P :-
+1
+"""
+'''
+for i in range(1,6):
+    if i==3:
+        pass
+    print(i)
+
+o/P
+1
+2
+3
+4
+5
+'''
+
+"""
+for i in range(1,6):
+    if i==3:
+        continue
+        print(i)
+o/p:
+1
+2
+4
+5
+"""
